f57799616c728fb4f7f89ae07427106c4ef4829b
Santiago Rios Valero <sriosv@correo.udsitrital.edu.co>
2022-12-05 13:44:13 -0500

    first commit

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const {
+  createTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks.controller");
+const router = Router();
+// const pool = require('../db');
+
+// router.get("/prueba", async (req, res)=>{
+//  const result = await pool.query('SELECT NOW()')
+//  console,console.log(result);
+//  res.json(result.rows[0].now)
+// })// create a task
+
+
+router.post("/tasks", createTask);
+
+router.get("/tasks", getAllTasks);
+
+router.get("/tasks/:id", getTask);
+
+router.put("/tasks/:id", updateTask);
+
+router.delete("/tasks/:id", deleteTask);
+
+module.exports = router;
