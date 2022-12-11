8eb3864d1c77814dfe626ce92ef7227e020a6965
Juan Sebastian Gonzalez Forero <72105720+Sgonzalezf97@users.noreply.github.com>
2022-12-10 19:46:56 -0500

    Se agrega el listado de clientes

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -2,23 +2,73 @@ import {
     Button,
     Card,
     CardContent,
-    CircularProgress,
-    Grid,
-    TextField,
     Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Clients = () => {
- 
-
-
+    const [clients, setClient] = useState([]);
+    const navigate = useNavigate();
+  
+    const loadClient = async () => {
+      const response = await fetch("http://localhost:4000/clients");
+      const data = await response.json();
+      console.log(data)
+      // comentar esto en caso de daño
+       setClient(data);
+    };
+  
+    useEffect(() => {
+      loadClient();
+    }, []);
+  
     return (
-        <div>
-            <h1></h1>
-        </div>
+      <>
+        <h1>Clientes</h1>
+        {
+            clients.map((clients) => (
+            <Card
+                style={{
+                marginBottom: ".7rem",
+                backgroundColor: "#1e272e",
+                }}
+            >
+                <CardContent
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}
+                >
+                <div
+                    style={{
+                    color: "white",
+                    }}
+                >
+                    {/* <Typography>{+clients.id_entidad}</Typography> */}
+                    <Typography>{"Nombres: "+clients.primer_nombre +" "+ clients.segundo_nombre }</Typography>
+                    {/* <Typography>{clients.segundo_nombre}</Typography> */}
+                    <Typography>{"Apellidos: "+clients.primer_apellido+" "+clients.segundo_apellido}</Typography>
+                    {/* <Typography>{clients.segundo_apellido}</Typography> */}
+                    <Typography>{"Fecha de nacimiento: "+clients.fecha_nacimiento}</Typography>
+                </div>
+                <div>
+                    <Button
+                    variant="contained"
+                    color="inherit"
+                    onClick={() => navigate(`/clients/${clients.id}/edit`)}
+                    >
+                    Editar
+                    </Button>
+                </div>
+                </CardContent>
+            </Card>
+            ))
+        }
+      </>
     );
-};
+  };
+  
+  export default Clients;
+  
 
-export default Clients;
