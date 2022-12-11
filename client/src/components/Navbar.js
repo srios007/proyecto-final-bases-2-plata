06769cec06471517daed925edc2f45532c62cf06
Juan Sebastian Gonzalez Forero <72105720+Sgonzalezf97@users.noreply.github.com>
2022-12-11 14:45:12 -0500

    [ADD] Logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { Container } from "@mui/material";
+import { Avatar, Container } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
@@ -17,6 +17,7 @@ export default function ButtonAppBar() {
       <AppBar position="static" color="transparent">
         <Container>
           <Toolbar>
+            <img src="https://github.com/srios007/proyecto-final-bases-2-plata/blob/add_client/client/src/components/Skaphe-logo.png" style={{marginRight: "1rem"}}/>
             <Typography variant="h6" component="div" sx={{ flexGrow: 2 }}>
               <Link to="/home" style={{ textDecoration: "none", color: '#eee',paddingRight: "3rem"}}>
                 AVALUOS
